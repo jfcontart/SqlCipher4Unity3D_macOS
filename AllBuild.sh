7697547484a6026b1b38ba216bce6fbec1ead4b7
JFC Kortex <jfcontart@idemobi.com>
2021-04-18 17:50:56 +0200

    [ADD] all build

---
+++ AllBuild.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+#---------------------------------------------------------------------------------------------
+
+./SQLCipherBuilt_macOS.sh 3.3.0
+
+./SQLCipherBuilt_macOS.sh 3.3.1
+
+./SQLCipherBuilt_macOS.sh 3.4.0
+
+./SQLCipherBuilt_macOS.sh 3.4.1
+
+./SQLCipherBuilt_macOS.sh 3.4.2
+
+./SQLCipherBuilt_macOS.sh 4.1.0
+
+./SQLCipherBuilt_macOS.sh 4.2.0
+
+./SQLCipherBuilt_macOS.sh 4.3.0
+
+./SQLCipherBuilt_macOS.sh 4.4.0
+
+./SQLCipherBuilt_macOS.sh 4.4.1
+
+./SQLCipherBuilt_macOS.sh 4.4.2
+
+./SQLCipherBuilt_macOS.sh 4.4.3
+
+#---------------------------------------------------------------------------------------------
+
